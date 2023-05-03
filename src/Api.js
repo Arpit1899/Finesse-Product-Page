aebc7c2a8e16c23b8c4ad51daccbb81e247022b3
Arpit1899 <79353277+Arpit1899@users.noreply.github.com>
2023-05-03 15:17:04 -0400

    Code cleanup

--- src/Api.js
+++ src/Api.js
@@ -1,20 +1,20 @@
 export const handleAddToFavorites = async (id, title, price) => {
-    try {
-      const response = await fetch("http://localhost:3003/add-to-favorites", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ id, title, price }),
-      });
-  
-      if (!response.ok) {
-        throw new Error("Failed to add product to favorites");
-      }
-  
-      return true;
-    } catch (error) {
-      return false;
+  try {
+    // Replace the URL below with the IP address on your network without changing the port number
+    const response = await fetch("http://192.168.1.229:3003/add-to-favorites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, title, price }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to add product to favorites");
     }
-  };
-  
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
